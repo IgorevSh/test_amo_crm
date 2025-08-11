eede74181df5998531b4710c8139f2577e3a29a2
Danila <shumd100@gmail.com>
2025-08-11 20:05:29 +0400

    test run

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import routes from './src/routes/index.js'
 import dotenv from 'dotenv';
-
 dotenv.config();
 const app = express();
-
-// Middleware
 app.use(cors());
 app.use(routes)
 const port = process.env.RUN_PORT || 3000
